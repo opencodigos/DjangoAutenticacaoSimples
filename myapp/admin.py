dc51c25edf7cab2e49073564f64c5ef8beb3af49
eticialima <leticialimacgi@gmail.com>
2022-12-27 18:17:44 -0300

    Atualiza Readme

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from myapp.models import MyProfile
+
 # Register your models here.
+admin.site.register(MyProfile)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
 
-# Create your models here.
+class MyProfile(models.Model):
+    user = models.OneToOneField(User, 
+						on_delete=models.CASCADE, related_name='profile')
+    description = models.CharField(max_length=100)
+
+
+@receiver(post_save, sender=User)
+def my_handler(sender, **kwargs):
+    """
+    Quando Criar um usuário no Django, vai rodar essa função
+    para criar uma instancia nesse modelo MyProfile no campo "user".
+    """
+    if kwargs.get('created', False):
+        MyProfile.objects.create(user=kwargs['instance'])
